18299081272999ee683548db5b93437192d9d0a1
Davi do Pain no gain <davi.lavorenti@escola.pr.gov.br>
2026-03-10 09:44:11 -0300

    Cadastro de aluno

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function salvar(){
+    let nome = document.getElementById("nome").value
+    console.log('nome:', nome)
+
+
+    let idade = document.getElementById("idade").value 
+    console.log('idade:', idade)
+}
